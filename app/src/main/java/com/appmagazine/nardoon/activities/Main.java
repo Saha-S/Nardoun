c165b9bb25414bd395a1aabb70198c4ea3341077
behroozhj <behrooz.jahromi@gmail.com>
2017-02-26 17:17:02 +0330

    toolbar completed

--- app/src/main/java/com/appmagazine/nardoon/activities/Main.java
+++ app/src/main/java/com/appmagazine/nardoon/activities/Main.java
@@ -173,13 +173,7 @@ public class Main extends AppCompatActivity
 
         } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
+        } else if (id == R.id.nav_manage) {}
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.END);
