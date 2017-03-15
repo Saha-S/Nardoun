1afde2d43c1f48af8f0ae2d5f5398535f349fcfd
nadia <nadiya6050@gmail.com>
2017-03-15 12:42:37 +0330

    new agahi

--- app/src/main/java/com/appmagazine/nardoon/activities/New.java
+++ app/src/main/java/com/appmagazine/nardoon/activities/New.java
@@ -1,8 +1,11 @@
 package com.appmagazine.nardoon.activities;
 
+        import android.content.Context;
         import android.content.Intent;
+        import android.os.AsyncTask;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.util.Log;
         import android.view.View;
         import android.widget.Button;
         import android.widget.EditText;
@@ -17,6 +20,13 @@ package com.appmagazine.nardoon.activities;
         import org.json.JSONArray;
         import org.json.JSONException;
         import org.json.JSONObject;
+
+        import java.io.BufferedReader;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
+        import java.io.OutputStreamWriter;
+        import java.net.HttpURLConnection;
+        import java.net.URL;
 
         import cz.msebera.android.httpclient.Header;
 
@@ -83,17 +93,22 @@ package com.appmagazine.nardoon.activities;
                 params.put("deviceid","2463");
                 params.put("devicemodel","jja");
                 params.put("location","ولی عصر");
-
+                Log.i("myurl" , App.urlApi+"agahis/");
                 client.post(App.urlApi+"agahis/", params, new AsyncHttpResponseHandler() {   // **************   get request  vase post: clinet.post qarar midim
                     @Override
                     public void onStart() {
                         // called before request is started
+
 
                         // loginpb1.setVisibility(View.VISIBLE);      *************** inja  progressbar faal mishe
                     }
                     @Override
                     public void onSuccess(int statusCode, Header[] headers, byte[] response) {
                         // called when response HTTP status is "200 OK" ************** inja vaqti successful shod code 200 daryaft kard mituni json parse koni
+                     //   String value = new String(response);
+                     //   System.out.println("Product Head Json: "+value);
+                    //    productHeaderJsonParser JsonParse=new productHeaderJsonParser();
+                     //   JsonParse.productHeaderJsonParserInput(value);
 
                         // loginpb1.setVisibility(View.INVISIBLE);
 
@@ -116,4 +131,33 @@ package com.appmagazine.nardoon.activities;
                     }
                 });
             }
+
+            public static String makePostRequest(String stringUrl, String payload,
+                                                 Context context) throws IOException {
+                URL url = new URL(stringUrl);
+                HttpURLConnection uc = (HttpURLConnection) url.openConnection();
+                String line;
+                StringBuffer jsonString = new StringBuffer();
+
+                uc.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                uc.setRequestMethod("POST");
+                uc.setDoInput(true);
+                uc.setInstanceFollowRedirects(false);
+                uc.connect();
+                OutputStreamWriter writer = new OutputStreamWriter(uc.getOutputStream(), "UTF-8");
+                writer.write(payload);
+                writer.close();
+                try {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(uc.getInputStream()));
+                    while((line = br.readLine()) != null){
+                        jsonString.append(line);
+                    }
+                    br.close();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                uc.disconnect();
+                return jsonString.toString();
+            }
+
         }
