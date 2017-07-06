09e3df7036df48d9c9db92835b5b9f83e5c4ecab
Nadia <nadiya6050@gmail.com>
2017-07-06 13:16:03 +0430

    telegram

--- app/src/main/java/com/appmagazine/nardoon/activities/Poshtibani.java
+++ app/src/main/java/com/appmagazine/nardoon/activities/Poshtibani.java
@@ -1,6 +1,8 @@
 package com.appmagazine.nardoon.activities;
 
+import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -18,6 +20,7 @@ public class Poshtibani extends AppCompatActivity {
         setContentView(R.layout.activity_poshtibani);
 
         TextView tvtitle = (TextView) findViewById(R.id.tv_mainpage_title);
+        TextView telegram = (TextView) findViewById(R.id.telegram);
         TextView appbarTitle = (TextView) findViewById(R.id.tv_appbar_title);
         ImageButton ibBack = (ImageButton) findViewById(R.id.ib_back);
         appbarTitle.setText("پشتیبانی");
@@ -33,6 +36,16 @@ public class Poshtibani extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        telegram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("http://t.me/nardoun96"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+
             }
         });
 
